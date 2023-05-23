a5bd711365b95644a48111c2ab143f770c070485
Dan <danh.nguyen@gumiviet.com>
2023-05-23 15:26:17 +0700

    surface reconstruction progress

--- main_surface_reconstruct_dup.py
+++ main_surface_reconstruct_dup.py
@@ -2,47 +2,63 @@
 # 樋の左右から取得した２点群の合成
 # （Open3D 0.12.0 使用: pip install open3d）
 #
-# 2021-01-12 hshimada@ice.ous.ac.jp
+# 2023-05-22 danhnt1105@gmail.com
 import concurrent
 from datetime import datetime
 import open3d as o3d
 import numpy as np
-import copy
 import sys
-import asyncio
+
+
+def surface_reconstruct(source, densities_threshold):
+
+    radius_normal = voxel_size * 2
+
+    source.estimate_normals(
+        o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
+    source.orient_normals_consistent_tangent_plane(100)
+
+    print('run Poisson surface reconstruction')
+    mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
+        source, depth=9)
+
+    densities = np.asarray(densities)
+    print('remove low density vertices')
+    vertices_to_remove = densities < np.quantile(densities, densities_threshold)
+    mesh.remove_vertices_by_mask(vertices_to_remove)
+
+    result = o3d.geometry.PointCloud()
+    result.points = mesh.vertices
+    result.colors = mesh.vertex_colors
+    result.normals = mesh.vertex_normals
+
+    return result
+
 
 ###########################################################################
 ###########################################################################
 ###########################################################################
 # メイン
 if __name__ == "__main__":
-    print("Process start time: ", datetime.now().strftime("%H:%M:%S"))
+    print("Process reconstruct start time: ", datetime.now().strftime("%H:%M:%S"))
     sys.stdin.reconfigure(encoding='utf-8')
 
     sys.stdout.reconfigure(encoding='utf-8')
 
-
-    # ダウンサンプリングのボクセルサイズ(m)
     voxel_size = 0.02
 
-    target_data = "./ShellTOICUbe.xyz"
-    target_pcd = o3d.io.read_point_cloud(target_data)
-    target_pcd.paint_uniform_color([1, 0.706, 0])
+    target_data = "./ShellTOICube.xyz"
+    output_data = "./ShellTOICube_reconstruct.xyz"
 
-    # radius_normal = 0.01
-    # target_pcd.estimate_normals(
-    #     o3d.geometry.KDTreeSearchParamHybrid(radius=radius_normal, max_nn=30))
-    # target_pcd.orient_normals_consistent_tangent_plane(100)
-    # # o3d.visualization.draw_geometries([target_pcd], point_show_normal=True)
-    # print('run Poisson surface reconstruction')
-    # with o3d.utility.VerbosityContextManager(
-    #         o3d.utility.VerbosityLevel.Debug) as cm:
-    #     mesh, densities = o3d.geometry.TriangleMesh.create_from_point_cloud_poisson(
-    #         target_pcd, depth=9)
-    # # o3d.visualization.draw_geometries([mesh])
-    # densities = np.asarray(densities)
-    # print('remove low density vertices')
-    # vertices_to_remove = densities < np.quantile(densities, 0.01)
-    # mesh.remove_vertices_by_mask(vertices_to_remove)
-    # print(mesh)
-    o3d.visualization.draw_geometries([target_pcd])
+    target_pcd = o3d.io.read_point_cloud(target_data)
+
+    pcd = surface_reconstruct(target_pcd, 0.01)
+    refine_pcd = surface_reconstruct(pcd, 0.01)
+    refine_pcd = surface_reconstruct(refine_pcd, 0.01)
+    # o3d.io.write_point_cloud(output_data, refine_pcd)
+    with open(output_data, 'w') as f:
+        for i, p in enumerate(refine_pcd.points):
+            f.write("%f %f %f %d %d %d\n" % (p[0], p[1], p[2],
+                                             int(refine_pcd.colors[i][0] * 255), int(refine_pcd.colors[i][1] * 255), int(refine_pcd.colors[i][2] * 255)))
+
+    print("Process reconstruct end time: ", datetime.now().strftime("%H:%M:%S"))
